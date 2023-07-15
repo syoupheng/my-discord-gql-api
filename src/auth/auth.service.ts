f8365164fd5b0107b3f9a0cea390d52c9a9f04c6
Pheng <do.syoupheng@gmail.com>
2023-07-15 22:03:41 +0200

    Add cookie domain

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -56,7 +56,8 @@ export class AuthService {
       expires: dayjs()
         .add(this.config.get('HTTP_ONLY_COOKIE_EXP_TIME') ?? 1, 'days')
         .toDate(),
-      maxAge: 60 * 60 * 24 * (this.config.get('HTTP_ONLY_COOKIE_EXP_TIME') ?? 1),
+      maxAge: 1000 * 60 * 60 * 24 * (this.config.get('HTTP_ONLY_COOKIE_EXP_TIME') ?? 1),
+      domain: this.config.get('NODE_ENV') === 'development' ? undefined : this.config.get('COOKIE_DOMAIN'),
     };
     return req.res.cookie('access_token', token, COOKIE_OPTIONS);
   }
